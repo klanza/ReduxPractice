f99f553caf19275fe9abb7dcb9ae0bc8fbdfe881
klanza <klanza91@gmail.com>
2018-09-11 23:40:00 -0700

    Created book reducer and book container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    { title: "You don't know JS" },
+    { title: 'Snow Crash' },
+    { title: 'A Song of Ice and Fire: A Game of Thrones' },
+    { title: 'Catch-22' }
+  ];
+};
