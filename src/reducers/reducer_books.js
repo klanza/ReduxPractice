d7873f61dbef158d1db4977d5a24ec196633f683
klanza <klanza91@gmail.com>
2018-09-23 19:41:55 -0700

    Built reducer for active book and book detail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    { title: "You don't know JS" },
-    { title: 'Snow Crash' },
-    { title: 'A Song of Ice and Fire: A Game of Thrones' },
-    { title: 'Catch-22' }
+    { title: "You don't know JS", pages: 153 },
+    { title: 'Snow Crash', pages: 303 },
+    { title: 'A Song of Ice and Fire: A Game of Thrones', pages: 546 },
+    { title: 'Catch-22', pages: 210 }
   ];
 };
